3379a0b5f7b88e53d47f399b3422f857a72756f7
odin11 <odinufa2014@gmail.com>
2022-01-17 00:36:10 +0300

    2 commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -22,8 +22,8 @@ public class Main {
     //task2
 
         int firstFriday = 5;
-        for (int j =firstFriday;j<=31;j = j + 7) {
-            System.out.println("Сегодня пятница, "+ j + "-е число. Необходимо подготовить отчет.");
+        for (int dayInMonth =firstFriday;dayInMonth<=31;dayInMonth = dayInMonth + 7) {
+            System.out.println("Сегодня пятница, "+ dayInMonth + "-е число. Необходимо подготовить отчет.");
         }
 
     //task3
@@ -31,9 +31,9 @@ public class Main {
         int currentYear = 2022;
         int start = currentYear - 200;
         int end = currentYear + 100;
-        for ( int y = start; y <= end; y++) {
-            if (y % 79 == 0) {
-                System.out.println(y);
+        for ( int year = start; year <= end; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
             }
         }
     }
